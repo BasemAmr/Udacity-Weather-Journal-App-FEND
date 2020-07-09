3286019a16c5896d57edc7adc972e28c9ab158ee
BasemAmr <67838239+BasemAmr@users.noreply.github.com>
2020-07-10 00:24:52 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,6 +1,8 @@
+// Global Variables
 const _date = document.getElementById('date');
 const _temp = document.getElementById('temp');
 const _content = document.getElementById('content');
+const _label = document.getElementById('entryLabel');
 
 // Personal API Key for OpenWeatherMap API
 const APINum = '8078fdbb772a5ceb4d5cffae1b5c3e9c';
@@ -10,25 +12,22 @@ const OWMURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+// Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', generateWeatherFunc);
+
+/* Function called by event listener */
 function generateWeatherFunc(event) {
     const zip = document.getElementById('zip').value;
     const UserFeelings = document.getElementById('feelings').value;
-    console.warn(feelings);
     
     getWeatherInfo(OWMURL, zip, APINum)
     .then(data => {
         // let date = new Date(data.dt * 1000)
         // let date_str = date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate();
-        postData('/add', {temperature: data.main.temp, date: newDate, feelings: UserFeelings});
-        updateUserInterface('/all');
+        postData('/data', {temperature: data.main.temp, date: newDate, feelings: UserFeelings});
+        updateUserInterface('/info');
     })
 };
-
-// Event listener to add function to existing HTML DOM element
-
-/* Function called by event listener */
-
 
 /* Function to GET Web API Data */
 const getWeatherInfo = async (url, zipNum, appId) => {
@@ -37,7 +36,7 @@ const getWeatherInfo = async (url, zipNum, appId) => {
         const data = await res.json();
         return data;
     } catch(error) {
-        console.log('ERROR', error);
+        console.error('ERROR', error);
     };
 };
 
@@ -56,7 +55,7 @@ const postData = async (url = '', data = {}) => {
         const newData = await response.json();
         return newData;
     } catch(error) {
-        console.log('error', error);
+        console.error('ERROR', error);
     };
 };
 
@@ -68,6 +67,8 @@ const updateUserInterface = async(url='') => {
         _date.innerHTML = UIData[0].date;
         _temp.innerHTML = UIData[0].temperature;
         _content.innerHTML = UIData[0].feelings;
+        _label.innerHTML = 'Most Recent Entry';
+        _label.innerHTML = _label.innerHTML.replace('y', 'y:');
     } catch(error) {
         console.error('ERROR', error);
     };
